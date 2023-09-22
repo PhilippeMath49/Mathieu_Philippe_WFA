7c61f8ec6208b12c40757c89dbab524c4bd3836d
PhilippeMath49 <philippe.mathieu@ynov.com>
2023-09-22 13:47:56 +0200

    fonction restart + mouvement joueur

--- Form1.cs
+++ Form1.cs
@@ -12,26 +12,103 @@ namespace WFA
 {
     public partial class Form1 : Form
     {
+
         bool goLeft, goRight, jumping, isGameOver;
+
         int jumpSpeed;
         int force;
         int score = 0;
-
-        
+        int playerSpeed = 7;
 
         int horizontalSpeed = 5;
         int verticalSpeed = 3;
-        
 
-       
+        int enemyOneSpeed = 5;
+        int enemyTwoSpeed = 3;
+
+
+
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void mainGameTimerEvent(object sender, EventArgs e)
+        private void MainGameTimerEvent(object sender, EventArgs e)
         {
-            if 
+            txtScore.Text = "Score: " + score;
+
+            player.Top += jumpSpeed;
+
+            if (goLeft == true)
+            {
+                player.Left -= playerSpeed;
+            }
+            if (goRight == true)
+            {
+                player.Left += playerSpeed;
+            }
+
+            if (jumping == true && force < 0)
+            {
+                jumping = false;
+            }
+
+            if (jumping == true)
+            {
+                jumpSpeed = -8;
+                force -= 1;
+            }
+            else
+            {
+                jumpSpeed = 10;
+            }
+
+            foreach (Control x in this.Controls)
+            {
+                if (x is PictureBox)
+                {
+
+
+                    if ((string)x.Tag == "platform")
+                    {
+                        if (player.Bounds.IntersectsWith(x.Bounds))
+                        {
+                            force = 8;
+                            player.Top = x.Top - player.Height;
+
+
+                            if ((string)x.Name == "horizontalPlatform" && goLeft == false || (string)x.Name == "horizontalPlatform" && goRight == false)
+                            {
+                                player.Left -= horizontalSpeed;
+                            }
+
+
+                        }
+
+                        x.BringToFront();
+
+                    }
+
+                    
+                    
+
+                }
+            }
+
+
+            
+
+
+            if (player.Top + player.Height > this.ClientSize.Height + 50)
+            {
+                gameTimer.Stop();
+                isGameOver = true;
+                txtScore.Text = "Score: " + score + Environment.NewLine + "You fell to your death!";
+            }
+
+            
+
+
         }
 
         private void KeyIsDown(object sender, KeyEventArgs e)
@@ -49,6 +126,7 @@ namespace WFA
                 jumping = true;
             }
         }
+
         private void KeyIsUp(object sender, KeyEventArgs e)
         {
             if (e.KeyCode == Keys.Left)
@@ -63,19 +141,49 @@ namespace WFA
             {
                 jumping = false;
             }
+
             if (e.KeyCode == Keys.Enter && isGameOver == true)
             {
                 RestartGame();
             }
+
+
         }
+        
+        private void RestartGame()
+        {
 
-    public class Player
-    {
-        int playerSpeed = 7;
-    }
+            jumping = false;
+            goLeft = false;
+            goRight = false;
+            isGameOver = false;
+            score = 0;
 
-    public class Ennemy
-    {
-        int ennemySpeed = 2;
+            txtScore.Text = "Score: " + score;
+
+            foreach (Control x in this.Controls)
+            {
+                if (x is PictureBox && x.Visible == false)
+                {
+                    x.Visible = true;
+                }
+            }
+        
+
+            // reset the position of player, platform and enemies
+
+            player.Left = 72;
+            player.Top = 656;
+
+            enemyOne.Left = 471;
+            enemyTwo.Left = 360;
+
+            horizontalPlatform.Left = 275;
+            verticalPlatform.Top = 581;
+
+            gameTimer.Start();
+
+
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WFA;
 
 namespace WFA
 {
-    internal static class Program
+    static class Program
     {
         /// <summary>
-        /// Point d'entrée principal de l'application.
+        /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
